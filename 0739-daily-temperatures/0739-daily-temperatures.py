eca709041bd033e1dd56a7e839b56d5ae09aa27a
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2026-04-05 17:38:24 +0530

    Time: 118 ms (26.57%), Space: 27.5 MB (99.77%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,10 +3,13 @@ class Solution:
         stack=[]
         ans = [0]*len(temperatures)
         day = 0
+
         while day < len(temperatures):
+
             while stack and temperatures[day] > temperatures[stack[-1]]:
                 ans[stack[-1]] = day - stack[-1]
                 stack.pop()
+
             stack.append(day)
             day += 1
         return ans
