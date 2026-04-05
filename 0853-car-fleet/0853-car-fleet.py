212c0be6967029bd34800f43e682c7fe45e98cf8
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2026-04-05 20:12:34 +0530

    Time: 140 ms (87.96%), Space: 41.7 MB (49.32%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,19 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        finishTime = [float('inf')] * len(position)
-        fleetLead = len(finishTime) -1
-        fleetCount = 1
-        sp = sorted(zip(position,speed))
+        fleetTime = 0
+        fleetCount = 0
+        sp = sorted(zip(position,speed),reverse=True)
 
-        for i in range (len(sp)):
-            p,s = sp[i]
-            finishTime[i] = (target - p)/s
-
-        for car in range (len(finishTime)-2,-1,-1):
-            if finishTime[car] > finishTime[fleetLead]:
+        for p,s in sp:
+            currentTime = (target - p)/s 
+            if currentTime > fleetTime:
                 fleetCount += 1
-                fleetLead = car
+                fleetTime = currentTime
 
         return fleetCount
 
-#nlogn can be better by combing both for loop into and direct cal without storing and fetching again
+#O(nlogn), O(1)
