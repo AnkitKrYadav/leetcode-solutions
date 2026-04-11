7ec4fa54439d221cbd6f007b37520af0c9ecc536
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2026-04-11 19:27:35 +0530

    Time: 172 ms (44.7%), Space: 20.7 MB (56.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,20 +1,20 @@
 class Solution:
     import math
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def fintime(speed):
+            time = 0
+            for i in piles:
+                time += math.ceil(i/speed) 
+            return time
+
         low,high=1,max(piles)
         while low < high:
             mid = (low+high)//2
-            if self.fintime(mid,piles) > h:
+            if fintime(mid) > h:
                 low = mid+1
             else:
                 high = mid
         return high
-            
-    
-    def fintime(self,speed,piles):
-        time = 0
-        for i in piles:
-            time += math.ceil(i/speed) 
-        return time
+      
 
 #O(nlogn)
