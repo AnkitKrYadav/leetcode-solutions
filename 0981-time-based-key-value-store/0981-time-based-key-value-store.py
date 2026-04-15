e2f396bb4a416eb6912cf730bf2f8b6bb0fd22be
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2026-04-15 22:24:09 +0530

    Time: 124 ms (59.56%), Space: 68.9 MB (84.8%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -3,11 +3,10 @@ import collections
 class TimeMap:
 
     def __init__(self):
-        # Using defaultdict(list) prevents KeyErrors when setting a new key
         self.store = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        # Just append the tuple; don't reassign to the result of append()
+
         self.store[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
