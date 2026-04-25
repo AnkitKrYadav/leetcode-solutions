ff520d94d68e475fb8bdf2f91d1d9e1732aeef41
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2026-04-25 22:52:54 +0530

    Time: 50 ms (43.65%), Space: 20.2 MB (5.26%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,29 +11,33 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head == None:
             return None
-        chead = Node(head.val)
-        l1 = head.next
-        l2  = chead
-        l2i = {0:chead}
-        pos,p = {head:0},1
+    
+        copy = Node(head.val)
+        l1,l2 = head.next, copy
+        l2i,l1pos,p = {0:copy},{head:0},1
+        
         #copy list node with next pointer only
         while l1:
-            pos[l1] = p
+            l1pos[l1] = p
             l2.next = Node(l1.val)
+
             l1 = l1.next
             l2 = l2.next
+
             l2i[p] = l2
+
             p += 1
+        #reset iterators    
         l1 = head
-        l2 = chead
+        l2 = copy
 
         # copy random pointers
         while l1:
             if l1.random != None:
-                k = pos[l1.random]
+                k = l1pos[l1.random]
                 l2.random = l2i[k]
             l1 = l1.next
             l2 = l2.next
-        return chead
-#O(n^2)                         
+        return copy
+#O(n)  can be also done using existing node in old2new dic and later joining them                       
         
