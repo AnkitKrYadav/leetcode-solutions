db59c45237b6d59b2dff41a6f454f6c5d7d1d844
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2026-08-30 15:45:54 +0530

    Time: 0 ms (100%), Space: 19.2 MB (80.4%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.py
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(1,len(nums)):
             if nums[i] != nums[i-1]:
                 di[nums[i]] = di.get(nums[i],0)+1
-        print(di)
+                
         for i in di.values():
             if i==1:
                 c+=1
